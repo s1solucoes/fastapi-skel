7a00a47e86d0fb10c2144392e60d4fdd71e582bc
Timóteo Ramos <timoteoramos@ymail.com>
2024-05-13 00:37:22 -0300

    feat: self hosted apm

--- skel/main.py
+++ skel/main.py
@@ -1,6 +1,12 @@
+from elasticapm.contrib.starlette import make_apm_client, ElasticAPM
 from fastapi import FastAPI
 
+apm = make_apm_client({
+    'SERVICE_NAME': 'skel',
+})
+
 app = FastAPI()
+app.add_middleware(ElasticAPM, client=apm)
 
 
 @app.get("/")
